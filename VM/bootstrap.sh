46392a3826bc33c4df303eacb0088e95a380ee2b
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-05-03 17:25:42 +0200

    Silence output

--- VM/bootstrap.sh
+++ VM/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Refreshing packages..."
-sudo apt-get -qq -y update --fix-missing
-sudo apt-get -qq -y install dos2unix
+sudo apt-get -qq -y update --fix-missing > /dev/null
+sudo apt-get -qq -y install dos2unix  > /dev/null
 
 # Enter into shared directory
 cd /var/www
@@ -28,12 +28,12 @@ sh ../setup_phpmyadmin.sh
 
 # Setup Program-O
 echo "Installing Program-O..."
-git clone https://github.com/DigiPlatMOOC/Program-O.git programo
-curl -H "Content-Type: application/x-www-form-urlencoded" -X POST -d 'botmaster_name=Botmaster&debugemail=botmaster%40example.org&bot_name=MyFirstBot&bot_desc=My+first+AIML+bot&bot_active=1&format=json&error_response=No+AIML+category+found.+This+is+a+Default+Response.&page=2&bot_id=1&error_response=No+AIML+category+found.+This+is+a+Default+Response.&action=Save&dbh=localhost&dbn=programo&dbPort=3306&dbu=botmaster&dbp=botmaster2016&time_zone_locale=Europe%2FBerlin&adm_dbu=botmaster&adm_dbp=botmaster2016&adm_dbp_confirm=botmaster2016&debug_level=4&debug_mode=1&save_state=database' http://127.0.0.1/programo/install/install_programo.php
+git clone https://github.com/DigiPlatMOOC/Program-O.git programo &> /dev/null
+curl -s -H "Content-Type: application/x-www-form-urlencoded" -X POST -d 'botmaster_name=Botmaster&debugemail=botmaster%40example.org&bot_name=MyFirstBot&bot_desc=My+first+AIML+bot&bot_active=1&format=json&error_response=No+AIML+category+found.+This+is+a+Default+Response.&page=2&bot_id=1&error_response=No+AIML+category+found.+This+is+a+Default+Response.&action=Save&dbh=localhost&dbn=programo&dbPort=3306&dbu=botmaster&dbp=botmaster2016&time_zone_locale=Europe%2FBerlin&adm_dbu=botmaster&adm_dbp=botmaster2016&adm_dbp_confirm=botmaster2016&debug_level=4&debug_mode=1&save_state=database' http://127.0.0.1/programo/install/install_programo.php > /dev/null
 
 # Download TelegramBotSample files
 echo "Downloading sample repository..."
-git clone https://github.com/DigiPlatMOOC/TelegramBotSample.git telegram-bot-sample
+git clone https://github.com/DigiPlatMOOC/TelegramBotSample.git telegram-bot-sample &> /dev/null
 
 # Exiting /public/ sub-directory
 popd > /dev/null
