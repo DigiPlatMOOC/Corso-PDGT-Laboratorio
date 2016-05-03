0dce1d308abc530e71bb6a6a5d31d2d5436c7627
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-05-03 16:55:55 +0200

    New fix attempt

--- VM/bootstrap.sh
+++ VM/bootstrap.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-sudo apt-get -y update --fix-missing
-sudo apt-get -y install dos2unix
+echo "Refreshing packages..."
+sudo apt-get -qq -y update --fix-missing
+sudo apt-get -qq -y install dos2unix
 
-#enter into shared directory and create log directory
-cd /var/www/public
+# Enter into shared directory
+cd /var/www
 
-#restore UNIX line ending to prevent Git autocrlf=true to lead to errors
-dos2unix *.sh
+# Restore UNIX line endings (prevents Git's autocrlf=true from creating installation errors)
+dos2unix -kq *.sh
 
-#setup mysql db and user login
+# Setup MySQL database credentials
+echo "Setting up database..."
 sh setup_db.sh testdb testuser testpassword
 sh setup_db.sh programo botmaster botmaster2016
 
-#setup phpMyAdmin
+# Entering /public/ sub-directory to setup working space
+pushd public > /dev/null
+
+# Setup phpMyAdmin
+echo "Installing phpMyAdmin..."
 wget -q https://files.phpmyadmin.net/phpMyAdmin/4.6.0/phpMyAdmin-4.6.0-all-languages.tar.gz
 tar xf phpMyAdmin-4.6.0-all-languages.tar.gz
 mv phpMyAdmin-4.6.0-all-languages phpmyadmin
 rm phpMyAdmin-4.6.0-all-languages.tar.gz
 sh ../setup_phpmyadmin.sh
 
-#setup Program-O
+# Setup Program-O
+echo "Installing Program-O..."
 git clone https://github.com/DigiPlatMOOC/Program-O.git programo
 curl -H "Content-Type: application/x-www-form-urlencoded" -X POST -d 'botmaster_name=Botmaster&debugemail=botmaster%40example.org&bot_name=MyFirstBot&bot_desc=My+first+AIML+bot&bot_active=1&format=json&error_response=No+AIML+category+found.+This+is+a+Default+Response.&page=2&bot_id=1&error_response=No+AIML+category+found.+This+is+a+Default+Response.&action=Save&dbh=localhost&dbn=programo&dbPort=3306&dbu=botmaster&dbp=botmaster2016&time_zone_locale=Europe%2FBerlin&adm_dbu=botmaster&adm_dbp=botmaster2016&adm_dbp_confirm=botmaster2016&debug_level=4&debug_mode=1&save_state=database' http://127.0.0.1/setup/programo/install/install_programo.php
 
-#download TelegramBotSample files
+# Download TelegramBotSample files
+echo "Downloading sample repository..."
 git clone https://github.com/DigiPlatMOOC/TelegramBotSample.git telegram-bot-sample
+
+# Exiting /public/ sub-directory
+popd > /dev/null
+
+echo "All set!"
