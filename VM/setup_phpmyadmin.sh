a586b4301739c85685bf01ee42692e8d254735be
Lorenz Cuno Klopfenstein <lck@klopfenstein.net>
2016-05-03 17:13:40 +0200

    Attempted fix to phpMyAdmin setup

--- VM/setup_phpmyadmin.sh
+++ VM/setup_phpmyadmin.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Executes from /var/www/public
+# PhpMyAdmin is extracted into /var/www/public/phpmyadmin
+
+# Spring cleaning (what for? who knows)
 rm -f config.sample.inc.php
 rm -f config.inc.php
 
-mv /var/www/public/setup/phpmyadmin/config.sample.inc.php .
+cp /var/www/public/phpmyadmin/config.sample.inc.php /var/www/public/phpmyadmin/config.inc.php
+mv /var/www/public/phpmyadmin/config.sample.inc.php /var/www/public/phpmyadmin/config.sample.inc.php.bk
 
+# Replace blowfish secret
 randomBlowfishSecret=`openssl rand -base64 32`;
-sed -e "s|cfg\['blowfish_secret'\] = ''|cfg['blowfish_secret'] = '$randomBlowfishSecret'|" config.sample.inc.php > config.inc.php
-echo "\$cfg['CheckConfigurationPermissions'] = false;" >> config.inc.php
+sed -e "s|cfg\['blowfish_secret'\] = ''|cfg['blowfish_secret'] = '$randomBlowfishSecret'|" -i /var/www/public/phpmyadmin/config.inc.php
 
-mv config.inc.php phpmyadmin/.
-mv config.sample.inc.php phpmyadmin/config.sample.inc.php.bk
+echo "\$cfg['CheckConfigurationPermissions'] = false;" >> /var/www/public/phpmyadmin/config.inc.php
